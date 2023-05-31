ec6a8bdf2e8db7279302eaa4a73a68c9d1cd6d7f
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-05-31 15:15:24 +0900

    FEAT:users, profiles 테이블 생성

---
+++ src/main/java/com/samsam/repository/ProfileRepository.java
@@ -0,0 +1,12 @@
+package com.samsam.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.samsam.vo.ProfileVO;
+import com.samsam.vo.UserVO;
+
+@Repository
+public interface ProfileRepository extends CrudRepository<ProfileVO, Integer>{
+
+}
