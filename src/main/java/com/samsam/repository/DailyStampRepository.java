256d7922e05817661044dcc07f1fe4e25f9d189d
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-05-31 17:42:24 +0900

    [지만] JPA 브이오, 레포  생성

---
+++ src/main/java/com/samsam/repository/DailyStampRepository.java
@@ -0,0 +1,10 @@
+package com.samsam.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.samsam.vo.DailyStampVO;
+
+//출석체크 레포
+public interface DailyStampRepository extends CrudRepository<DailyStampVO, Integer>{
+
+}
---
+++ src/main/java/com/samsam/vo/FollowId.java
@@ -0,0 +1,31 @@
+package com.samsam.vo;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FollowId implements Serializable{//복합키 생성
+	//두개의 user_no를 받아와 복합키 생성
+	@ManyToOne
+	@JoinColumn(name ="follow_start",nullable = false)
+	private UserVO followStart;
+	 
+	
+	@ManyToOne 
+	@JoinColumn(name ="follow_end",nullable = false )
+	private UserVO followEnd;
+	
+}
