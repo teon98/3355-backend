06ef0d4bd2007fbd4ee1fa544e233dc6e3f6d8cf
KKangBro <zlddbblz@naver.com>
2023-06-08 18:02:32 +0900

    6/8

--- src/main/java/com/samsam/controller/CardRestController.java
+++ src/main/java/com/samsam/controller/CardRestController.java
@@ -1,10 +1,22 @@
 package com.samsam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/card")
-public class CardRestController {
+import com.samsam.service.CardService;
 
+@RestController
+@RequestMapping("/home")
+public class CardRestController {
+	
+	@Autowired
+	CardService cardService;
+	
+	@GetMapping("/pay")
+	public String barcodeScan(@RequestParam String storeNo) {
+		return cardService.barcodeScan(storeNo);
+	}
 }
