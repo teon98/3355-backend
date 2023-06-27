4d0dc234ecaff75bef757b6921c1b007061384eb
teon98 <k55416325@gmail.com>
2023-06-27 12:39:06 +0900

    팔로우 팔로워 요청, 취소 api 제작

--- src/main/java/com/samsam/repository/FollowRepository.java
+++ src/main/java/com/samsam/repository/FollowRepository.java
@@ -14,4 +14,7 @@ public interface FollowRepository extends CrudRepository<FollowVO, FollowId>{
 	
 	@Query(value = "select follow_end from follows where follow_start = ?1", nativeQuery = true)
 	List<Integer> findByFollowStart(Integer uid);
+	
+	@Query(value = "select count(*) from follows where follow_start =?1 and follow_end=?2", nativeQuery = true)
+	int findByEachOther(Integer uid1, Integer uid2);
 }
